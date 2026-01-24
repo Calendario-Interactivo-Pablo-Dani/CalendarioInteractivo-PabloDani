cb4022f17c2620240c85eb0d633a421fbe478eb3
Pablo S <pasadi06@educantabria.es>
2026-01-24 13:54:03 +0100

    Ajustes layouts login y register V.2

--- android/Planify/app/src/main/java/com/example/planify/MainActivity.java
+++ android/Planify/app/src/main/java/com/example/planify/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.planify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View.OnClickListener listenerRegistro=new View.OnClickListener() {
+
+        public void onClick(View view) {
+            Intent i=new Intent(getApplicationContext(), Register.class);
+            startActivity(i);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bRegister=(Button) findViewById(R.id.button_registro);
+        bRegister.setOnClickListener(listenerRegistro);
     }
 }
