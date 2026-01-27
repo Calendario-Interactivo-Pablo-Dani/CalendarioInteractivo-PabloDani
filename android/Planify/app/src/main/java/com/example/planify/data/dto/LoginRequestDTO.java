5aa2c9066367d5d3e9f447053dec4980d2096720
Daniel-tech-sudo <danielhprieto7@gmail.com>
2026-01-27 13:28:02 +0100

    DTO login y network

--- android/Planify/app/src/main/java/com/example/planify/data/dto/LoginRequestDTO.java
+++ android/Planify/app/src/main/java/com/example/planify/data/dto/LoginRequestDTO.java
@@ -1,4 +1,16 @@
 package com.example.planify.data.dto;
 
 public class LoginRequestDTO {
+    private String email;
+    private String password;
+    public LoginRequestDTO(String email, String password){
+        this.email = email;
+        this.password = password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
 }
---
+++ android/Planify/app/src/main/java/com/example/planify/data/dto/LoginResponseDTO.java
@@ -0,0 +1,30 @@
+package com.example.planify.data.dto;
+
+public class LoginResponseDTO {
+    private Integer id;
+    private String nombre;
+    private String username;
+    private String email;
+    private String telefono;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+}
