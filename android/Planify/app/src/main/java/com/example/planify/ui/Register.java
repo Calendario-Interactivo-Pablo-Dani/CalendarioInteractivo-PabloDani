99a84e9ca2e2f16d53f914fdc38e53df2fbea731
Pablo Samperio <pasadi06@educantabria.es>
2026-01-28 12:56:41 +0100

    Añadiendo listener pantalla register

--- android/Planify/app/src/main/java/com/example/planify/ui/Register.java
+++ android/Planify/app/src/main/java/com/example/planify/ui/Register.java
@@ -1,6 +1,9 @@
 package com.example.planify.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +12,24 @@ import com.example.planify.R;
 public class Register extends AppCompatActivity {
 
 
+    Button bRegister;
+
+
+    private View.OnClickListener listenerEntrar=new View.OnClickListener() {
+
+
+        public void onClick(View view) {
+            Intent i=new Intent(getApplicationContext(), MarcoGeneral.class);
+            startActivity(i);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_registro);
+
+        bRegister=(Button) findViewById(R.id.button_registro);
+        bRegister.setOnClickListener(listenerEntrar);
     }
 }
