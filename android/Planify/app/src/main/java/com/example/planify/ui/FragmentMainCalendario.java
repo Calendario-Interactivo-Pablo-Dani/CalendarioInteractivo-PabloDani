9162bdd50f66d384ef6636f9c601954cb65bc96a
Pablo Samperio <pasadi06@educantabria.es>
2026-01-28 13:50:16 +0100

    barra inferior tareas version vertical, login ya completado, nombre recolocado

--- android/Planify/app/src/main/java/com/example/planify/ui/FragmentMainCalendario.java
+++ android/Planify/app/src/main/java/com/example/planify/ui/FragmentMainCalendario.java
@@ -45,8 +45,31 @@ public class FragmentMainCalendario extends Fragment {
         Tarea t2 = new Tarea();
         t2.setNombre("Avanzar TFG");
 
+
+        Tarea t3 = new Tarea();
+        t1.setNombre("Estudiar PDM");
+
+        Tarea t4 = new Tarea();
+        t2.setNombre("Avanzar TFG");
+        Tarea t5 = new Tarea();
+        t1.setNombre("Estudiar PDM");
+
+        Tarea t6 = new Tarea();
+        t2.setNombre("Avanzar TFG");
+        Tarea t7 = new Tarea();
+        t1.setNombre("Estudiar PDM");
+
+        Tarea t8 = new Tarea();
+        t2.setNombre("Avanzar TFG");
+
         tareas.add(t1);
         tareas.add(t2);
+        tareas.add(t3);
+        tareas.add(t4);
+        tareas.add(t5);
+        tareas.add(t6);
+        tareas.add(t7);
+        tareas.add(t8);
 
         // 4️⃣ Creamos el adapter
         eventAdapter = new EventAdapter(tareas);
@@ -55,7 +78,7 @@ public class FragmentMainCalendario extends Fragment {
         recyclerView.setLayoutManager(
                 new LinearLayoutManager(
                         getContext(),
-                        LinearLayoutManager.HORIZONTAL,
+                        LinearLayoutManager.VERTICAL,
                         false
                 )
         );
