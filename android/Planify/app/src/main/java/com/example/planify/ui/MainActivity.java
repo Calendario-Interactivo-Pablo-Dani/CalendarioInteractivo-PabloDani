0be902cb41d55d19601c52597ebd51401c1e6521
Pablo Samperio <pasadi06@educantabria.es>
2026-02-02 08:51:10 +0100

    .

--- android/Planify/app/src/main/java/com/example/planify/ui/MainActivity.java
+++ android/Planify/app/src/main/java/com/example/planify/ui/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    //esto aun esta sin flitros(que haya campos escritos) es solo para ir probando
+    //esto aun esta sin flitros(que haya campos escritos) es solo para ir probandoa
     private View.OnClickListener listenerLogin=new View.OnClickListener() {
         public void onClick(View view) {
             String email = txEmail.getText().toString();
